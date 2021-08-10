08e721d223b346eb2b33750f8747e96066db0ebe
Yurii Ohlii <jukbq77@gmail.com>
2021-08-10 21:58:22 +0300

    end work

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let uaer_array = [];
 function render() {
     let table = '<tr>';
     $.each(uaer_array, function (key, data) {
+        let i = key;
         table += '<th>';
         table += key += 1;
         table += '</th>';
@@ -9,10 +10,10 @@ function render() {
         table += '<td>' + data['password'] + '</td>';
         table += '<td>' + data['male'] + '</td>';
         table += '<td>';
-        table += '<button type="button" onclick="edit()" class="btn btn-warning">Edit</button>';
+        table += '<button type="button" onclick="edit(' + i + ')" class="btn btn-warning">Edit</button>';
         table += '</td>';
         table += '<td>';
-        table += '<button type="button" onclick="delete()" class="btn btn-danger">Delete</button>';
+        table += '<button type="button" onclick="deleteUser(' + i + ')" class="btn btn-danger deleteUser" >Delete</button>';
         table += '</td>';
         table += '</tr>';
     });
@@ -29,15 +30,20 @@ class User {
         this.email1 = uEmail;
     }
     push_array() {
-        console.log(`${this.login}, ${this.pass}, ${this.email1}`);
         let person = {
             "name": this.login,
             "password": this.pass,
             "male": this.email1
         };
         uaer_array.push(person);
-        console.log(person);
-        console.log('vfccbd', uaer_array);
+    }
+    push_array_index(i) {
+        let person = {
+            "name": this.login,
+            "password": this.pass,
+            "male": this.email1
+        };
+        uaer_array.splice(i, 0, person);
     }
 }
 ;
@@ -51,4 +57,42 @@ function add_user() {
     $('#pass').val('');
     $('#email1').val('');
     render();
+}
+function deleteUser(i) {
+    $(".tab_body").empty();
+    let arr_index = i;
+    uaer_array.splice(i, 1);
+    render();
+}
+let userIndex = '';
+function edit(i) {
+    $.each(uaer_array, function (key, data) {
+        if (key === i) {
+            $('#login').val(data['name']);
+            $('#pass').val(data['password']);
+            $('#email1').val(data['male']);
+        }
+    });
+    userIndex = i;
+    $('.add_user').addClass('hide_button');
+    $('.edit_user').addClass('showe_button');
+}
+;
+function saveEditUser() {
+    let login = $('#login').val();
+    let pass = $('#pass').val();
+    let email1 = $('#email1').val();
+    let i = userIndex;
+    $(".tab_body").empty();
+    uaer_array.splice(i, 1);
+    let reg = new User(`${login}`, `${pass}`, `${email1}`);
+    reg.push_array_index(i);
+    $('#login').val('');
+    $('#pass').val('');
+    $('#email1').val('');
+    render();
+    $('.add_user').removeClass('hide_button');
+    $('.edit_user').removeClass('showe_button');
+    console.log(i);
+    console.log(uaer_array);
 }
--- main.ts
+++ main.ts
@@ -1,39 +1,33 @@
 
 let uaer_array: any[] = [];
 
+/*------------------------------Заповнення таблиці-----------------------*/
+
 function render(): void {
-   let table = '<tr>';
+    let table = '<tr>';
     $.each(uaer_array, function (key, data) {
+        let i = key
         table += '<th>'
-        table +=  key += 1
-        table +=   '</th>'
-        table +=  '<td>'+ data['name'] +  '</td>' 
-        table += '<td>' + data['password'] +  '</td>'
-        table += '<td>' + data['male'] +  '</td>'
-        table += '<td>' 
-        table += '<button type="button" onclick="edit()" class="btn btn-warning">Edit</button>'
-        table +=  '</td>'
-        table += '<td>' 
-        table += '<button type="button" onclick="delete()" class="btn btn-danger">Delete</button>'
-        table +=  '</td>'
-       table += '</tr>'
+        table += key += 1
+        table += '</th>'
+        table += '<td>' + data['name'] + '</td>'
+        table += '<td>' + data['password'] + '</td>'
+        table += '<td>' + data['male'] + '</td>'
+        table += '<td>'
+        table += '<button type="button" onclick="edit(' + i + ')" class="btn btn-warning">Edit</button>'
+        table += '</td>'
+        table += '<td>'
+        table += '<button type="button" onclick="deleteUser(' + i + ')" class="btn btn-danger deleteUser" >Delete</button>'
+        table += '</td>'
+        table += '</tr>'
+
     });
-      
-       
-        
-        $('.tab_body').html(table);
-     /*    console.log(key += 1);
-        console.log(data['name']);
-        console.log(data['password']);
-        console.log(data['male']); */
-     
-      
-        
+    $('.tab_body').html(table);
+
+
 };
 
-
-
-
+/*----------------------------обєкт user--------------------------------*/
 class User {
     public login: string;
     public pass: any;
@@ -44,20 +38,25 @@ class User {
         this.email1 = uEmail;
     }
     public push_array(): any {
-        console.log(`${this.login}, ${this.pass}, ${this.email1}`);
         let person = {
             "name": this.login,
             "password": this.pass,
             "male": this.email1
         }
         uaer_array.push(person)
-        console.log(person);
-        console.log('vfccbd', uaer_array);
-        
+    }
+    public push_array_index(i: number): any {
+        let person = {
+            "name": this.login,
+            "password": this.pass,
+            "male": this.email1
+        }
+        uaer_array.splice(i, 0, person)
     }
 };
 
-function add_user() {
+/*--------------------------Кнопка add_user------------------------*/
+function add_user(): void {
     let login = $('#login').val();
     let pass = $('#pass').val();
     let email1 = $('#email1').val();
@@ -66,8 +65,65 @@ function add_user() {
     reg.push_array()
 
     $('#login').val('');
-   $('#pass').val('');
+    $('#pass').val('');
     $('#email1').val('');
 
     render()
+}
+
+
+/*--------------------------Кнопка delete------------------------*/
+
+function deleteUser(i: any): void {
+    $(".tab_body").empty();
+    let arr_index: any = i
+    uaer_array.splice(i, 1)
+    render()
+
+
+}
+
+/*--------------------------Кнорка Edit---------------------------*/
+
+let userIndex: any = ''
+function edit(i: any): void {
+
+    $.each(uaer_array, function (key, data) {
+        if (key === i) {
+            $('#login').val(data['name']);
+            $('#pass').val(data['password']);
+            $('#email1').val(data['male']);
+        }
+    })
+    userIndex = i;
+    $('.add_user').addClass('hide_button')
+    $('.edit_user').addClass('showe_button')
+
+
+
+
+}; 
+
+
+function saveEditUser(): void {
+    let login = $('#login').val();
+    let pass = $('#pass').val();
+    let email1 = $('#email1').val();
+    let i = userIndex
+    $(".tab_body").empty();
+    uaer_array.splice(i, 1)
+    let reg = new User(`${login}`, `${pass}`, `${email1}`)
+    reg.push_array_index(i)
+
+    $('#login').val('');
+    $('#pass').val('');
+    $('#email1').val('');
+
+    render()
+    $('.add_user').removeClass('hide_button')
+    $('.edit_user').removeClass('showe_button')
+  
+    console.log(i);
+    console.log(uaer_array);
+
 }
